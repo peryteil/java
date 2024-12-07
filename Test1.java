22c2dd88fa1ed61e0fab051d5ae6164046845196
peryteil <peryteil@gmail.com>
2024-12-07 20:53:17 +0900

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hellp World");
+        System.out.println("안녕하세요?");
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(123+345);
+    }
+}
